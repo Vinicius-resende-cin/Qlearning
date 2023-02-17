cea70a775fe3f65a12091bb831c4bc743758a9a1
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-02-17 18:19:11 -0300

    training phase - tired (reaches objective for each 500~ or so steps in the best cases)

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import connection as cn
 import os
+from random import randint, random as rand
 
 port = 2037
 s = cn.connect(port)
@@ -19,62 +20,85 @@ def read_table() -> list:
             
             table.append(row) # inclui na tabela local
 
-    string_table = '\n'.join(str(row) for row in table)
-    print(f"Tabela inicial = {string_table}")
+    string_table = '\n'.join(f"{row[0]:.6f} {row[1]:.6f} {row[2]:.6f}" for row in table)
+    print(f"Tabela inicial =\n{string_table}")
     return table
 
 
 def write_table(q_table: list):
     """Escreve a q_table atualizada no arquivo de resultado"""
-    os.remove('temp.txt')
-    with open('./temp.txt', 'w') as new_t:
+    os.remove('./resultado.txt')
+    with open('./resultado.txt', 'w') as new_t:
         for line in q_table:
-            row = ' '.join(f'{l:.6f}' for l in line) + '\n'
+            row = ' '.join(f'{l}' for l in line) + '\n'
             new_t.write(row)
 
             
-def do_action(q_table:list, state: str, lrate: float, dfactor: float) -> tuple[str, int, int]:
-    """Obtém o estado atual em sua forma binária e retorna o novo estado, a ação executada e sua recompensa"""
+def do_action(q_table:list, state: str, randomness: float) -> tuple:
+    """Obtém o estado atual e retorna ele mesmo, a ação executada, o novo estado e a recompensa"""
     state_row = q_table[int(state, 2)] # localiza estado na q table
+    r = rand() # gera valor aleatorio
 
     #1 =============================== Escolher uma ação e executá-la
-    action = [
-        m[0]
-        for m in enumerate(state_row)
-        if m[1] == max(state_row)
-    ][-1] # escolhe a última ação dentre as máximas (para priorizar o pulo)
+    if r <= randomness:
+        action = randint(0, len(state_row) - 1) # escolhe uma ação aleatória
+    else:
+        action = [
+            m[0]
+            for m in enumerate(state_row)
+            if m[1] == max(state_row)
+        ][-1] # escolhe a melhor ação
 
     action_name = action_names[action] # converte a ação para string
-    print(f"Estado: {int(state, 2)}, Ação: {action_name}")
+    print('{' + f"Estado: {int(state, 2)}, Ação: {action_name}", end='')
 
     #2 =============================== Observar o novo estado
     new_state, reward = cn.get_state_reward(s, action_name) # recebe novo estado
+    return state_row, action, new_state, reward
+
+
+def update_table(state_row: list, action: int, new_state: str, reward: float, lrate: float, dfactor: float):
+    """Aplica a recompensa na q_table"""
     new_state_row = q_table[int(new_state, 2)] # localiza o novo estado na q table
 
     #3 =============================== Aplicar a recompensa recebida
     state_row[action] += lrate * (reward + dfactor * max(new_state_row) - state_row[action])
-    return new_state, action, reward
+    print(f", Recompensa: {reward}" + '}')
 
 
-def generate_policy(q_table: list, lrate: float, dfactor: float, n_times: int, initial_state: str = '0000000'):
-    """Executa o loop de avançar entre estados n vezes"""
+def do_and_update(q_table: list, state: str, lrate: float, dfactor: float, randomness: float):
+    """Executa a ação, atualiza a q_table e retorna o novo estado e a recompensa"""
+    state_row, action, new_state, reward = do_action(q_table, state, randomness)
+    update_table(state_row, action, new_state, reward, lrate, dfactor)
+    return new_state, reward
 
-    state, _, _ = do_action(q_table, initial_state, lrate, dfactor) # Executa a primeira ação
 
-    # Loop é executado n_vezes
+def generate_policy(q_table: list, lrate: float, dfactor: float, randomness: float, n_times: int, initial_state: str = '0000000'):
+    """Percorre o ambiente até um estado final n vezes"""
     for i in range(n_times):
-        print(f"Passo {i + 1}:")
-        state, _, _ = do_action(q_table, state, lrate, dfactor)
+        print(f"\nIteração {i + 1}:")
+
+        state, reward = do_and_update(q_table, initial_state, lrate, dfactor, randomness) # executa a primeira ação
+        counter = 0
+        while reward != 300: # explora até chegar num estado terminal
+            counter += 1
+            print(f"Passo {counter}:", end=' ')
+            state, reward = do_and_update(q_table, state, lrate, dfactor, randomness)
 
 
-def explore(q_table:list, lr: float, df: float, n_times: int):
-    """Explora o ambiente e atualiza a q_table"""
-    generate_policy(q_table, lr, df, n_times)
+def explore(q_table:list, lr: float, df: float, rd: float, n_times: int, initial_state: str = '0000000'):
+    """Explora o ambiente e atualiza o arquivo da q_table"""
+    generate_policy(q_table, lr, df, rd, n_times, initial_state)
     write_table(q_table)
 
 
-def apply_policy(q_table: list):
-    pass
+def apply_policy(q_table: list, initial_state: str = '0000000'):
+    _, _, state, reward = do_action(q_table, initial_state, 0)
+    counter = 0
+    while reward != 300:
+        counter += 1
+        print('}\n' + f"Passo {counter}:", end=' ')
+        _, _, state, reward = do_action(q_table, state, 0)
         
 
 if __name__ == "__main__":
@@ -82,11 +106,14 @@ if __name__ == "__main__":
 
     mode = int(input("Digite 1 para explorar o ambiente, 0 para executar a política: "))
 
+    initial_state = '01010' + '00'
+
     if mode:
         learning_rate = float(input("Digite a taxa de aprendizagem: "))
         discount_factor = float(input("Digite o fator de desconto: "))
-        n_times = int(input("Digite a quantidade de passos: "))
+        randomness = float(input("Digite a frequencia de ações aleatórias: "))
+        n_times = int(input("Digite a quantidade de iterações: "))
 
-        explore(q_table, learning_rate, discount_factor, n_times)
+        explore(q_table, learning_rate, discount_factor, randomness, n_times, initial_state)
     else:
-        apply_policy(q_table)
+        apply_policy(q_table, initial_state)
